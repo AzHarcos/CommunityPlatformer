39534ad60a9a928d8e2436cb246dc31164054679
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-08-05 14:59:31 +0200

    create player const file

---
+++ LupusCommunityProject/Assets/Physics/Player/PlayerConst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerConst {
+
+    public const float GRAVITY = 9.81f;
+
+    public const bool THIS_IS_A_TEST_VALUE = true;
+
+}
