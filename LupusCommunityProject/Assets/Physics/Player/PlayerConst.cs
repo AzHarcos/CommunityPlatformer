a70cb0b432dcb251e5fb0940ec023ae77c14f8bf
AzHarcos <luc718@hotmail.de>
2021-08-06 18:21:35 +0200

    added placeholder sprites + animations, updated movement script for walking and jumping

--- LupusCommunityProject/Assets/Physics/Player/PlayerConst.cs
+++ LupusCommunityProject/Assets/Physics/Player/PlayerConst.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public static class PlayerConst {
 
-    public const float GRAVITY = 9.81f;
+    public const float GRAVITY = 5f;
 
     public const float SNEAKING_ACCEL = 5f;
     public const float sneakingMaxSpeed = 2f;
+
+    public const float JUMP_ACCEL = 15f;
 
 }
--- LupusCommunityProject/Assets/Physics/Player/PlayerMovement.cs
+++ LupusCommunityProject/Assets/Physics/Player/PlayerMovement.cs
@@ -5,37 +5,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private float PlayerSpeedX;
-    private float PlayerSpeedY;
+    // physics object, used to set gravity + velocity
+    private Rigidbody2D rb;
+    // animation object, used to set animation flags
+    private Animator animator;
+    // sprite object, used to adjust sprite
+    private SpriteRenderer spriteRenderer;
 
-    private float PlayerAccelX;
-
-
+    // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = PlayerConst.GRAVITY;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update() {
-        float dt = Time.deltaTime;
-
-        ProcessInput();
-        UpdatePlayer(dt);
+        // gets direction (-1. 0 or 1 on keyboard, any value between -1 and 1 on stick inputs)
+        float direction = Input.GetAxisRaw("Horizontal");
+        UpdateVelocity(direction);
+        UpdateAnimations(direction);
     }
 
-    private void UpdatePlayer(float dt) {
-        if (PlayerSpeedX < PlayerConst.sneakingMaxSpeed) {
-            PlayerSpeedX += PlayerAccelX * dt;
-        } else {
-            PlayerSpeedX = PlayerConst.sneakingMaxSpeed;
+    // updates the players x and y velocity
+    private void UpdateVelocity(float direction) {
+        float velocityX = direction * PlayerConst.SNEAKING_ACCEL;
+        float velocityY = rb.velocity.y;
+        if (Input.GetButtonDown("Jump")) {
+            velocityY = PlayerConst.JUMP_ACCEL;
         }
-
-        transform.position += new Vector3(PlayerSpeedX * dt, PlayerSpeedY * dt, 0);
+        rb.velocity = new Vector2(velocityX, velocityY);
     }
 
-    void ProcessInput () {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            PlayerAccelX = PlayerConst.SNEAKING_ACCEL;
+    // updates the current animation state
+    private void UpdateAnimations(float direction) {
+        if (direction == 0f) {
+            animator.SetBool("moving", false);
+        } else {
+            animator.SetBool("moving", true);
+            spriteRenderer.flipX = direction < 0f;
         }
     }
 }
