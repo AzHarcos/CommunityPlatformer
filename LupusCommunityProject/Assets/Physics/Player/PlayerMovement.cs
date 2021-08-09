17b03c5baac25a9e00fbf22012f05772cf3a26ee
DESKTOP-0I11N5A\Felix <felixkoenig11@web.de>
2021-08-09 17:45:21 +0200

    added an onGround check and is now only able to jump once

--- LupusCommunityProject/Assets/Physics/Player/PlayerMovement.cs
+++ LupusCommunityProject/Assets/Physics/Player/PlayerMovement.cs
@@ -11,7 +11,15 @@ public class PlayerMovement : MonoBehaviour {
     private Animator animator;
     // sprite object, used to adjust sprite
     private SpriteRenderer spriteRenderer;
+    // used to check if the player is touching the ground
+    private bool onGround;
+    private bool canJump;
 
+    public Transform groundCheck;
+
+    public float groundCheckRadius;
+
+    public LayerMask whatIsGround;
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -26,18 +34,31 @@ public class PlayerMovement : MonoBehaviour {
         float direction = Input.GetAxisRaw("Horizontal");
         UpdateVelocity(direction);
         UpdateAnimations(direction);
+        CheckSurroundings();
+        CheckIfCanJump();
     }
 
     // updates the players x and y velocity
     private void UpdateVelocity(float direction) {
         float velocityX = direction * PlayerConst.SNEAKING_ACCEL;
         float velocityY = rb.velocity.y;
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && canJump) {
             velocityY = PlayerConst.JUMP_ACCEL;
-        }
+            }
         rb.velocity = new Vector2(velocityX, velocityY);
     }
+    private void CheckSurroundings() {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
 
+    private void CheckIfCanJump() {
+        if ((onGround) && (rb.velocity.y <= 0.01f)) {
+            canJump = true;
+        }
+        else {
+            canJump = false;
+        }
+    }
     // updates the current animation state
     private void UpdateAnimations(float direction) {
         if (direction == 0f) {
@@ -46,5 +67,9 @@ public class PlayerMovement : MonoBehaviour {
             animator.SetBool("moving", true);
             spriteRenderer.flipX = direction < 0f;
         }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
