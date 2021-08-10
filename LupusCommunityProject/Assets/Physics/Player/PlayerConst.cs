b7e32f5590ccd96f2399ca5dbae2344265cf37df
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-08-10 10:00:02 +0200

    documentation

--- LupusCommunityProject/Assets/Physics/Player/PlayerConst.cs
+++ LupusCommunityProject/Assets/Physics/Player/PlayerConst.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public static class PlayerConst {
 
+    // the gravitational force pulling the player down each frame
     public const float GRAVITY = 5f;
 
+    // the forces applied when holding a direction
     public const float SNEAKING_ACCEL = 20f;
     public const float SNEAKING_MAX_SPEED = 10f;
 
+    // jump height/speed
     public const float JUMP_ACCEL = 10f;
-
+    // max jump hold time
     public const float MAX_JUMP_HOLD_TIME = .2f;
 
 }
